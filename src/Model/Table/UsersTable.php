e3d296a242f2461c16fe42e3cbbc26747d7f8be2
Marcio Vennan <marciovenann@gmail.com>
2016-04-21 23:43:20 -0300

    corrindo metodos isAdmin e isAdminUser

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -137,73 +137,25 @@ class UsersTable extends Table
         return $context['data']['passwordConfirm'] == $context['data']['newPassword'];
     }
 
-    // public static function isAdmin( $profile_id = null )
-    // {
+    public static function isAdmin( $profile_id = null )
+    {
 
-    //     if( !$profile_id )
-    //         $profile_id = AuthComponent::user( 'profile_id' );
+        if( !$profile_id )
+            $profile_id = $this->Auth->user("profile_id");
 
-    //     return $profile_id == Configure::read( 'AdminProfileId' );
-    // }
+        return $profile_id == Configure::read( 'AdminProfileId' );
+    }
 
-    // public static function isAdminUser( $user_id = null )
-    // {
+    public static function isAdminUser( $user_id = null )
+    {
 
-    //     if( !$user_id )
-    //         $user_id = AuthComponent::user( 'id' );
+        if( !$user_id )
+            $user_id = $this->Auth->user("id"));
 
-    //     return $user_id == Configure::read( 'AdminUserId' );
-    // }
+        return $user_id == Configure::read( 'AdminUserId' );
+    }
 
-    // ----------------------------------------
-    //  * Callbacks
-    //  ----------------------------------------
-
-    // public function beforeValidate( $options = array() )
-    // {
-
-    //     if( array_key_exists( 'pass_switched', $options ) ){
-
-    //         $this->_passSwitched = $options[ 'pass_switched' ];
-
-    //         if( !$this->_passSwitched ){
-
-    //             $this->validate[ 'newPassword' ][ 'alphanumeric' ][ 'allowEmpty' ] = false;
-    //             $this->validate[ 'newPassword' ][ 'between' ][ 'allowEmpty' ] = false;
-    //         }
-    //     }
-
-    //     return true;
-    // }
-    
-    // public function beforeSave( $options = array() )
-    // {
-
-    //     if ( !$this->id )
-    //         $this->data[ $this->name ][ 'password' ] = AuthComponent::password( '123456' );
-
-    //     elseif ( !empty( $this->data[ $this->name ][ 'newPassword' ] ) ) {
-
-    //         $this->data[ $this->name ][ 'password' ] = AuthComponent::password( $this->data[ $this->name ][ 'newPassword' ] );
-    //         unset( $this->data[ $this->name ][ 'newPassword' ] );
-
-    //         if( !empty( $this->data[ $this->name ][ 'passwordConfirm' ] ) )
-    //             unset( $this->data[ $this->name ][ 'passwordConfirm' ] );
-
-    //         if( isset( $this->_passSwitched ) )
-    //             if( !$this->_passSwitched )
-    //                 $this->_passSwitched = $this->data[ $this->name ][ 'pass_switched' ] = '1';
-
-    //     } elseif ( isset( $this->data[ $this->name ][ 'password' ] ) )
-    //         unset( $this->data[ $this->name ][ 'password' ] );
-
-    //     if ( isset( $this->data[ $this->name ][ 'pass_switched' ] ) )
-    //         if( !$this->data[ $this->name ][ 'pass_switched' ] )
-    //             unset( $this->data[ $this->name ][ 'pass_switched' ] );
-        
-    //     return true;
-    // }
-
+   
     public function beforeSave(Event $event, Entity $entity) 
     {   
         if ($entity->isNew()) {
