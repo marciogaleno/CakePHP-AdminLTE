d3cbabf55d2b632e9190e243309f588a49d7ce3e
Marcio Vennan <marciovenann@gmail.com>
2016-04-22 00:49:22 -0300

    alterando class do form para o tamanho dos inputs

--- config/app_form.php
+++ config/app_form.php
@@ -4,7 +4,7 @@ return [
 	'formStart' => '<form class="form-horizontal" {{attrs}}><div class="box-body">',
 	'formEnd' => '</gmp_div()></form>',
 	'inputContainer' => '<div class="form-group">{{content}}</div>',
-	'input' => '<div class="col-sm-5"><input type="{{type}}" name="{{name}}" class="form-control" {{attrs}}/></div>',
+	'input' => '<div class="col-xs-5"><input type="{{type}}" name="{{name}}" class="form-control" {{attrs}}/></div>',
 	'label' => '<label class="col-sm-2 control-label" {{attrs}}>{{text}}</label>',
 	'submitContainer' => '<div class="box-footer"><a href="{{url_button_cancel}}" class="btn btn-default pull-right" style="margin-left:5px">{{name_button_cancel}}</a>{{content}}</div>',
 	'inputSubmit' => '<input class="btn btn-success pull-right" type="{{type}}"{{attrs}}/>',
